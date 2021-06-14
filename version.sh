c449c9344a67a3a79cb036c6901661e1501d12a5
Pascal Watteel <pascal@watteel.be>
2021-06-14 15:51:11 +0400

    move commands to script

--- version.sh
+++ version.sh
@@ -1,3 +1,5 @@
 echo "Finding latest Sabnzbd release and set it as a env.variable to use during docher build"
-
+apt update -y
+apt install curl grep -y
 version=$(curl --silent "https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+echo "$version" >> vars.env
