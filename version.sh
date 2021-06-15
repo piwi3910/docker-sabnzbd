446b7bb05fb3bf5fb0d69f6b8b3531bd71c18a75
Pascal Watteel <pascal@watteel.be>
2021-06-15 18:36:04 +0400

    add option to force no cache rebuild

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-echo "Finding latest Sabnzbd release and set it as a env.variable to use during docher build"
+echo "Finding latest Sabnzbd release and set it as a env.variable to use during docker build"
 apt update -y
 apt install curl grep -y
 version=$(curl --silent "https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
