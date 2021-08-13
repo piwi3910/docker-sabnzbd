883b8416a6d64c0e55b072fadc5d87063b981e71
Watteel Pascal <pascal@watteel.be>
2021-08-13 11:22:40 +0400

    Move (#1)
    * move to jenkins file nad k8s build
    
    * test
    
    * test
    
    * add sleep to ubuntu image
    
    * add version arg to docker build
    
    * test
    
    * test
    
    * correct vars
    
    * vars
    
    * cleanup and readme
    
    * add docker README.md push
    
    * test with readme push
    
    * test
    
    * test
    
    * test
    
    * move version command over to jenkins file
    
    * test
    
    * remove version script
    
    * escape
    
    * test
    
    * test
    
    * delete hidden chars
    
    * remove hidden chars
    
    * disable readme push for now
    
    * fix
    
    * fix
    
    * always pull latest base image
    
    * make jenkins file universal

--- version.sh
+++
@@ -1,6 +0,0 @@
-echo "Finding latest Sabnzbd release and set it as a env.variable to use during docker build"
-apt update -y
-apt install curl grep -y
-version=$(curl --silent "https://api.github.com/repos/sabnzbd/sabnzbd/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-echo "version=$version" >> vars.env
-
